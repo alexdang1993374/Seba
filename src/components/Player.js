4d32eb47b38e0608364bf0587efb360ba9408687
unknown <alexdang1993374@gmail.com>
2021-01-27 23:42:38 +0700

    slider functional

--- src/components/Player.js
+++ src/components/Player.js
@@ -36,6 +36,14 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     );
   };
 
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({
+      ...songInfo,
+      currentTime: e.target.value,
+    });
+  };
+
   //State
   const [songInfo, setSongInfo] = useState({
     currentTime: null,
@@ -50,6 +58,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
           min={0}
           max={songInfo.duration}
           value={songInfo.currentTime}
+          onChange={dragHandler}
           type="range"
         />
         <p>{getTime(songInfo.duration)}</p>
