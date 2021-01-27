bab8be623f77c83c945d4fedb978d3d4d74a4ee6
unknown <alexdang1993374@gmail.com>
2021-01-27 22:48:42 +0700

    wrote data file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 //Components
 import Player from "./components/Player";
 import Song from "./components/Songs";
+//Data
+import data from "./data";
 //Style
 import "./styles/app.scss";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+
   return (
     <div className="App">
       <Song />
