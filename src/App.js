4c75186fca1a99f86821d2db12c9363aff80951c
unknown <alexdang1993374@gmail.com>
2021-01-27 21:36:03 +0700

    made components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+//Components
+import Player from "./components/Player";
+import Song from "./components/Songs";
 
 function App() {
   return (
     <div className="App">
-      <h1>Seba</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
