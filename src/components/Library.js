c2e071f247bf0b3dd6d392a9a6da1bc690b1a782
unknown <alexdang1993374@gmail.com>
2021-01-28 01:34:50 +0700

    can select different songs

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,17 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
-          <LibrarySong song={song} />
+          <LibrarySong
+            setCurrentSong={setCurrentSong}
+            song={song}
+            key={song.id}
+          />
         ))}
       </div>
     </div>
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, setCurrentSong }) => {
+  const songSelectHandler = () => {
+    setCurrentSong(song);
+  };
+
   return (
-    <div className="library-song">
+    <div onClick={songSelectHandler} className="library-song">
       <img src={song.cover} alt={song.name} />
       <div className="song-description">
         <h3>{song.name}</h3>
